2f422a866eae7e9a1fc9290170d67a4b8ce2fa82
Jaloliddin <bosimovjaloliddin@gmail.com>
2023-07-22 23:51:06 +0500

    add navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Container, Wrapp, Items, Item } from "./style";
+import { Button, Dropdown, Space } from "antd";
+
+import {
+  Container,
+  Wrapp,
+  Items,
+  Item,
+  Category,
+  WrapSearch,
+  Search,
+  User,
+  Icons,
+  DropdownWrapp,
+  DropdownItems,
+  DropdownItem,
+} from "./style";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const menu = (
+    <DropdownWrapp>
+      <DropdownItems>
+        <DropdownItem>Sut, qatiq, yog', tusum</DropdownItem>
+        <DropdownItem>Poliz ekinlari</DropdownItem>
+        <DropdownItem>Mevalar</DropdownItem>
+        <DropdownItem>Qolgan maxsulotlar</DropdownItem>
+      </DropdownItems>
+    </DropdownWrapp>
+  );
   return (
     <>
       <Container>
@@ -13,13 +38,34 @@ const Navbar = () => {
             <Item>Logo</Item>
           </Items>
           <Items>
-            <Item onClick={() => navigate("/home")}>Home</Item>
-            <Item>Get Product</Item>
-            <Item>Add Product</Item>
-            <Item onClick={() => navigate("/contact")}>Contact</Item>
+            <Dropdown dropdownRender={() => menu} placement={"bottomCenter"}>
+              <Category>
+                <Icons.Menu />
+                <span>Kategory</span>
+              </Category>
+            </Dropdown>
+            <WrapSearch>
+              <Search placeholder="Search" />
+              <Icons.Search />
+            </WrapSearch>
           </Items>
           <Items>
-            <Item>Login</Item>
+            <Item>
+              <Icons.Love />
+              <span>Tanlangan</span>
+            </Item>
+            <Item>
+              <Icons.Get />
+              <span>Buyurtma</span>
+            </Item>
+            <Item>
+              <Icons.Shopping />
+              <span>Savat</span>
+            </Item>
+            <User>
+              <img src="#" alt="" />
+              <span>Jalol</span>
+            </User>
           </Items>
         </Wrapp>
       </Container>
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -1,16 +1,24 @@
-import { styled } from "styled-components";
+import { styled, css } from "styled-components";
+import { ReactComponent as love } from "../../assets/icons/love.svg";
+import { ReactComponent as get } from "../../assets/icons/get.svg";
+import { ReactComponent as shopping } from "../../assets/icons/shopping.svg";
+import { ReactComponent as menu } from "../../assets/icons/menu.svg";
+import { ReactComponent as search } from "../../assets/icons/search.svg";
 
 const Container = styled.div`
-  background: blue;
+  /* background: blue; */
+  background: #fff;
+  box-shadow: 2px 4px 8px 0px rgba(0, 0, 0, 0.1);
 `;
 const Wrapp = styled.ul`
-  max-width: 1440px;
-  padding: 0 130px;
-  height: 64px;
   display: flex;
-  justify-content: space-around;
+  max-width: 1440px;
+  height: 72px;
+  padding: 0px 116px;
+  justify-content: center;
   align-items: center;
-  color: white;
+  gap: 40px;
+  flex-shrink: 0;
   margin: auto;
 `;
 const Items = styled.ul`
@@ -18,12 +26,124 @@ const Items = styled.ul`
   justify-content: space-between;
   gap: 30px;
 `;
-const Item = styled.li`
-  display: inline-block;
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
   cursor: pointer;
   &:active {
     transform: scale(0.98);
   }
+  color: #414141;
+  span {
+    color: #414141;
+    text-align: center;
+    font-family: "Rubik";
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 150%;
+  }
+`;
+const Category = styled.div`
+  width: 140px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  border-radius: 4px;
+  background: #70c05b;
+  cursor: pointer;
+  span {
+    color: #fff;
+    text-align: center;
+    font-family: Rubik;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 150%;
+  }
+`;
+const WrapSearch = styled.div`
+  position: relative;
+  width: 375px;
+  height: 40px;
+`;
+const User = styled.div``;
+const Search = styled.input`
+  width: 100%;
+  height: 100%;
+  padding: 8px;
+  border-radius: 4px;
+  border: 1px solid #70c05b;
+  background: #fff;
+  outline: none;
 `;
 
-export { Container, Wrapp, Items, Item };
+const styleIcons = css`
+  width: 24px;
+  height: 24px;
+  flex-shrink: 0;
+  cursor: pointer;
+`;
+const Icons = styled.div``;
+Icons.Love = styled(love)`
+  ${styleIcons}
+`;
+Icons.Get = styled(get)`
+  ${styleIcons}
+`;
+Icons.Shopping = styled(shopping)`
+  ${styleIcons}
+`;
+Icons.Search = styled(search)`
+  position: absolute;
+  right: 8px;
+  top: 8px;
+  ${styleIcons}
+`;
+Icons.Menu = styled(menu)`
+  ${styleIcons}
+`;
+
+// Drobdown style
+const DropdownWrapp = styled.div`
+  background: #fff;
+  margin-top: 18px;
+  padding: 12px 24px;
+`;
+const DropdownItems = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+const DropdownItem = styled.span`
+  align-self: stretch;
+  color: #414141;
+  font-family: "Rubik";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 150%;
+  cursor: pointer;
+  &:hover {
+    color: #f63;
+  }
+`;
+
+export {
+  Container,
+  Wrapp,
+  Items,
+  Item,
+  Category,
+  Search,
+  WrapSearch,
+  User,
+  Icons,
+  DropdownWrapp,
+  DropdownItems,
+  DropdownItem,
+};
